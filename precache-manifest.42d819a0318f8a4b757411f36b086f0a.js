14a8e51de13f62e9a7cae286ee31c7c5796de535
AlexReactV18 <152198548+AlexReactV18@users.noreply.github.com>
2023-11-27 10:48:13 -0600

    First Commit

---
+++ precache-manifest.42d819a0318f8a4b757411f36b086f0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0216d478c39534375b2a6fc977b8623c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72a39f2fed9ac6d85f76",
+    "url": "/static/css/main.918bbed2.chunk.css"
+  },
+  {
+    "revision": "6532be015995c78e7321",
+    "url": "/static/js/2.ebf43ca6.chunk.js"
+  },
+  {
+    "revision": "3fa3eaf425018ded1d331225d0003673",
+    "url": "/static/js/2.ebf43ca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a39f2fed9ac6d85f76",
+    "url": "/static/js/main.065eb134.chunk.js"
+  },
+  {
+    "revision": "377d7b3ee5f4ba45818d",
+    "url": "/static/js/runtime-main.7844606b.js"
+  }
+]);
